5773238c1ca3d08c58b042b3d6ed3098b01ff46f
nikisalli <niki.salli@gmail.com>
2021-03-03 18:56:19 +0100

    new packet class and deep clean up

--- lib/minecraft/minecraft.h
+++ lib/minecraft/minecraft.h
@@ -2,60 +2,117 @@
 #define MINECRAFT_H
 
 #include <Arduino.h>
+#include <mutex>
 
-class minecraft{
-    String username;
-    String server_url;
-    int server_port;
-    
-
+class packet{
     public:
-        minecraft(String _username, String _url, uint16_t _port, Stream* __S);
+    uint8_t buffer[6000];
+    uint32_t index = 0;
+    bool compression_enabled;
+    Stream* S;
+    std::mutex * mtx;
 
-        Stream* S;
-        bool compression_enabled = 0;
-        int compression_treshold = 0;
-        bool writing = 0;
-        double x = 0;
-        double y = 0;
-        double z = 0;
-        float yaw = 0;
-        float pitch = 0;
-        bool onGround = true;
-        bool teleported = false;
-        float health = 0;
-        int food = 0;
-        float food_sat = 0;
-        
-        int timeout = 100;
+    packet(Stream* __S, std::mutex * _mtx, bool _compression_enabled){
+        S = __S;
+        compression_enabled = _compression_enabled;
+        mtx = _mtx;
+    }
 
-        void handle             ();  // this stream is the logging port not the web socket!!
-        void teleportConfirm    (int id);
-        void setRotation        (float yaw, float pitch, bool ground);
-        void keepAlive          (uint64_t id);
-        void request            ();
-        void ping               (uint64_t num);
-        void loginStart         ();
-        void writeChat          (String text);
-        void handShake          (uint8_t state);
-        void clientStatus       (uint8_t state);
+    void write(uint8_t val);
+    void write(uint8_t * buf, size_t size);
+    void writePacket();
 
-        float readFloat         ();
-        double readDouble       ();
-        int readVarInt          ();
-        String readString       ();
-        long readLong           ();
-        int VarIntLength        (int val);
-    private:
-        void writeDouble        (double value);
-        void writeFloat         (float value);
-        void writeVarInt        (int16_t value);
-        void writeVarLong       (int64_t value);
-        void writeString        (String str);
-        void writeLong          (uint64_t num);
-        void writeUnsignedShort (uint16_t num);
+    // write to buf
+    void writeDouble        (double value);
+    void writeFloat         (float value);
+    void writeVarInt        (int32_t value);
+    void writeVarLong       (int64_t value);
+    void writeString        (String str);
+    void writeUnsignedLong  (uint64_t num);
+    void writeUnsignedShort (uint16_t num);
+    void writeUnsignedByte  (uint8_t num);
+    void writeLong          (int64_t num);
+    void writeInt           (int32_t num);
+    void writeShort         (int16_t num);
+    void writeByte          (int8_t num);
+    void writeBoolean       (uint8_t val);
+    void writeUUID          (int user_id);
+
+    // write to server
+    void serverWriteVarInt  (int32_t value);
 };
 
-int lsr(int x, int n);
+class minecraft{
+    public:
+    minecraft(String _username, String _url, uint16_t _port, Stream* __S);
+
+    String username;
+    String server_url;
+    uint32_t server_port;
+    std::mutex * mtx;
+    Stream* S;
+    int id;
+    bool compression_enabled = 0;
+    int compression_treshold = 0;
+    bool writing = 0;
+    double x = 0;
+    double y = 0;
+    double z = 0;
+    float yaw = 0;
+    float pitch = 0;
+    int yaw_i = 0;
+    int pitch_i = 0;
+    bool onGround = true;
+    float health = 0;
+    int food = 0;
+    float food_sat = 0;
+    uint8_t buf[50000];
+    
+    int timeout = 100;
+
+    void handle();
+
+    // clientbound
+    void readSpawnPlayer         ();
+    void readFoodAndSat          ();
+    void readPlayerPosAndLook    ();
+    void readBlockDestroy        ();
+    void readKeepAlive           ();
+    void readChat                ();
+    void readSetCompressionThres ();
+    void readDisconnected        ();
+
+    void writeHandle             ();  // this stream is the logging port not the web socket!!
+    void writeTeleportConfirm    (int id);
+    void writeSetRotation        (float yaw, float pitch, bool ground);
+    void writeKeepAlive          (uint64_t id);
+    void writeRequest            ();
+    void writePing               ();
+    void writeLoginStart         ();
+    void writeChat               (String text);
+    void writeHandShake          (uint8_t state);
+    void writeClientStatus       (uint8_t state);
+
+    void loginfo            (String msg);
+    void logerr             (String msg);
+    void login              (String msg);
+    void logout             (String msg);
+
+    float readFloat         ();
+    double readDouble       ();
+    int32_t readVarInt      ();
+    String readString       ();
+    int64_t readLong        ();
+    uint16_t readUnsignedShort();
+    uint32_t VarIntLength   (int32_t val);
+    uint8_t readByte        ();
+    bool readBool           ();
+    uint64_t readUUID       ();
+
+    void writeLength        (uint32_t length);
+};
+
+int32_t lsr(int32_t x, uint32_t n);
+float fmap(float x, float in_min, float in_max, float out_min, float out_max);
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,12 @@
 
 TaskHandle_t listener;
 WiFiClient client;
-minecraft mc (username, server_url, server_port, &client);
+minecraft mc(username, server_url, server_port, &client);
 
 void update( void * pvParameters ){
-    mc.handle();
+    while(true){
+        mc.handle();
+    }
 }
 
 void setup() {
@@ -27,8 +29,6 @@ void setup() {
     Serial.println("Connected to the WiFi network");
     Serial.println(WiFi.localIP());
 
-    delay(1000);
-
     const char* buf;
     buf = server_url.c_str();
 
@@ -38,41 +38,25 @@ void setup() {
         return;
     }
 
-    client.setNoDelay(true);
-    client.setTimeout(2);
+    xTaskCreatePinnedToCore(update, "listener", 100000, NULL, 10, &listener, 0);
 
-    xTaskCreatePinnedToCore(update, "listener", 100000, NULL, 2, &listener, 0);
-
-    delay(1000);
-    mc.handShake(2);
-    Serial.println("[INFO] -> handShake packet sent");
-    delay(1000);
-    //mc.ping(client, 1);
-
-    mc.loginStart();
-    Serial.println("[INFO] -> login_start packet sent");
-    delay(1000);
-
-    //mc.clientStatus(0);
-    //Serial.println("[INFO] -> client_status packet sent");
-    //delay(100);
-
-    /*mc.writeChat("/login nikk");
-    Serial.println("[INFO] -> logging in as nikbot");
-    delay(200);*/
+    mc.writeHandShake(2);
+    vTaskDelay(pdMS_TO_TICKS(500));
+    mc.writeLoginStart();
+    vTaskDelay(pdMS_TO_TICKS(500));
 
     mc.writeChat("test");
     Serial.println("[INFO] -> writing to chat");
-    // vTaskDelay(pdMS_TO_TICKS(1000));
 }
 
 void loop(){
-    mc.yaw+=5;
+    mc.yaw+=10;
     if(mc.yaw > 360){
         mc.yaw = 0;
     }
-    mc.setRotation(mc.yaw, mc.pitch, mc.onGround);
-    delay(50);
-    Serial.println("[INFO] # buf: " + String(client.available()/1024.0) + "kB Heap: " + String(ESP.getFreeHeap()/1024.0) + "kB");
+    mc.writeSetRotation(mc.yaw, mc.pitch, mc.onGround);
+    vTaskDelay(pdMS_TO_TICKS(50));
+
+    //Serial.println("[INFO] # buf: " + String(client.available()/1024.0) + "kB Heap: " + String(ESP.getFreeHeap()/1024.0) + "kB");
     //mc.writeChat(client, "[nik INFO] Heap: " + String(ESP.getFreeHeap()/1024.0) + "kB || buf: " + String(client.available()/1024.0) + "kB");
 }
